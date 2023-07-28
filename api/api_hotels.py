b484a40c8558510b6a0fb36ab27dab211a1ff358
Evgeny Ageev <e@bixfleet.com>
2023-07-29 00:21:09 +0300

    Added request for extra Property photos

--- api/api_hotels.py
+++ api/api_hotels.py
@@ -4,6 +4,7 @@ import logging
 import requests
 from datetime import datetime
 from telebot import TeleBot, types
+from telebot.types import InputMediaPhoto
 from config_data.config import *
 from utils.constants import *
 from database.db import DBUtil
@@ -19,9 +20,7 @@ class ApiRequests:
         Function for sending query for getting list of cities
         """
         # print('get_cities func')
-        if not query:
-            raise Exception('Query isnt set')
-
+        bot.send_chat_action(chat_id=chat_id, action='typing')
         headers = {
             "Content-Type": "application/json",
             "X-RapidAPI-Key": RAPID_API_KEY,
@@ -65,6 +64,7 @@ class ApiRequests:
         Function for sending query for getting list of hotels
         """
         # print('get_hotels func')
+        bot.send_chat_action(chat_id=chat_id, action='typing')
         req = db.get_current_request(chat_id)[0]
         start_date = datetime.strptime(req[5], DATE_FORMAT)
         end_date = datetime.strptime(req[6], DATE_FORMAT)
@@ -93,6 +93,7 @@ class ApiRequests:
             "filters": {"availableFilter": "SHOW_AVAILABLE_ONLY"}
         }
         # print('PARAMS =', params)
+        result = []
         try:
             response = requests.post(PROPERTIES_LIST_URL, headers=headers, json=params)
             if not response.status_code == 200:
@@ -107,7 +108,9 @@ class ApiRequests:
 
             i = 1
             for prop in props:
+                prop_elem = {}
                 hotel_id = prop['id']
+                prop_elem['id'] = hotel_id
                 prop_name = prop['name']
                 price = round(prop['price']['lead']['amount'], 2)
                 distance = prop['destinationInfo']['distanceFromDestination']['value']
@@ -120,13 +123,13 @@ class ApiRequests:
                     neighborhood_name = prop['destinationInfo']['distanceFromMessaging']
                 price_diff = round(days_diff * price, 2)
 
-                text = f'{i}) Название отеля: {prop_name}\n' \
+                prop_elem['text'] = f'{i}) Название отеля: {prop_name}\n' \
                        f'От центра: {distance} км\n' \
                        f'Название района: {neighborhood_name}\n' \
                        f'Цена за сутки: ${price}\n' \
                        f'Всего за {days_diff} дней: ${price_diff}\n' \
                        f'{link}'
-                bot.send_message(chat_id, text, disable_web_page_preview=True)
+
                 db.add_hotel(hotel_id, prop_name, region_id, distance, price, link)
                 i += 1
 
@@ -136,9 +139,57 @@ class ApiRequests:
                     # some properties have no image, so we need to check it
                     if prop_image and prop_image['image'] and prop_image['image']['url']:
                         url = prop_image['image']['url']
-                        bot.send_photo(chat_id, url)
+                        prop_elem['images'] = []
+                        prop_elem['images'].append(url)
                         db.add_hotel_image(hotel_id, url, self.logger)
                     else:
                         self.logger.error('Cant get Property Image!')
+
+                result.append(prop_elem)
         except Exception as ex:
             self.logger.error(ex)
+
+        detail_request_needed = req[9] > 1
+        try:
+            for prop_elem in result:
+                bot.send_message(chat_id, prop_elem['text'], disable_web_page_preview=True)
+                # if we need just 1 photo, we'll send it right away
+                if not detail_request_needed:
+                    bot.send_photo(chat_id, prop_elem['images'][0])
+                else:
+                    photos = self.get_prop_photos(prop_elem['id'], req[9])
+                    bot.send_media_group(chat_id, photos)
+        except Exception as ex:
+            self.logger.error(ex)
+
+    def get_prop_photos(self, prop_id: int, max_cnt: int):
+        # print('get_prop_photos func')
+        headers = {
+            "Content-Type": "application/json",
+            "X-RapidAPI-Key": RAPID_API_KEY,
+            "X-RapidAPI-Host": RAPID_API_HOST
+        }
+        params = {
+            "currency": "USD",
+            "eapid": 1,
+            "locale": "ru_RU",
+            "siteId": 300000001,
+            "propertyId": str(prop_id)
+        }
+
+        response = requests.post(PROPERTIES_DETAIL_URL, headers=headers, json=params)
+        if not response.status_code == 200:
+            self.logger.error(f'Status = {response.status_code}')
+            return
+
+        i = 0
+        photos = []
+        json_data = json.loads(response.text)
+        images = json_data['data']['propertyInfo']['propertyGallery']['images']
+        for image in images:
+            if i >= max_cnt:
+                break
+
+            photos.append(InputMediaPhoto(image['image']['url']))
+            i += 1
+        return photos
